ea155448344f40a5b839de03b31d1a40e5d9e0d8
fauz-del <fauzeeyatagbalaya27@gmail.com>
2026-01-23 13:16:50 +0100

    Rename project to TerraBloom, fix image paths

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// vite.config.js
-var vite_1 = require("vite");
-var plugin_react_1 = require("@vitejs/plugin-react");
-exports.default = (0, vite_1.defineConfig)({
-    base: '/TerraBloom/',
-    plugins: [(0, plugin_react_1.default)()],
-});
